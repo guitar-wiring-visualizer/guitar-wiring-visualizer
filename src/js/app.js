df3a1bd88446a8acc8f111d74e4839d94eaad85a
David W Martines <d5172@yahoo.com>
2026-01-23 15:58:41 -0600

    Create app layout.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
+const setupApp = () => {
+
+    const diagramStage = setupDiagram();
+    const libraryStage = setupLibrary();
+}
 
 
 
@@ -32,7 +37,64 @@ const runDemo = () => {
     stage.add(layer);
 }
 
+
+
+
+
+function setupDiagram() {
+    const diagramStage = new Konva.Stage({
+        container: 'diagram',
+        width: 1000,
+        height: 1000,
+    });
+
+    const diagramLayer = new Konva.Layer();
+
+    // create our sample shape
+    const circle = new Konva.Circle({
+        x: diagramStage.width() / 2,
+        y: diagramStage.height() / 2,
+        radius: 70,
+        fill: 'red',
+        stroke: 'black',
+        strokeWidth: 4,
+    });
+    circle.draggable('true');
+    diagramLayer.add(circle);
+
+    diagramStage.add(diagramLayer);
+
+    return diagramLayer;
+}
+
+function setupLibrary() {
+    const libraryStage = new Konva.Stage({
+        container: 'library',
+        width: 200,
+        height: 1000,
+    });
+
+    const libraryLayer = new Konva.Layer();
+
+    // create our sample shape
+    const circle = new Konva.Circle({
+        x: libraryStage.width() / 2,
+        y: libraryStage.height() / 2,
+        radius: 70,
+        fill: 'red',
+        stroke: 'black',
+        strokeWidth: 4,
+    });
+    circle.draggable('true');
+    libraryLayer.add(circle);
+
+    libraryStage.add(libraryLayer);
+
+    return libraryStage;
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("loader").hidden = true;
-    runDemo();
+    setupApp();
 });
