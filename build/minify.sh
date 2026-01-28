b3603ba02a6661b8aa7641896476cd79ed098d43
David W Martines <d5172@yahoo.com>
2026-01-28 17:50:11 -0600

    try new build/minify

---
+++ build/minify.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -l
+
+npm i minify@15.0.1 -g
+apt-get update
+apt-get -y install moreutils
+
+find . -type f -not -path "*/vendor/*" \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | while read fname
+    do
+    # minify ${fname} | sponge ${fname}
+    echo ${fname}
+    done
