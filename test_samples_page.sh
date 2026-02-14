03311dd950db63268e947b4c6f67a4004e63de42
David W Martines <d5172@yahoo.com>
2026-02-14 08:29:25 -0600

    Fix md to html params.

--- test_samples_page.sh
+++ test_samples_page.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-markdown-to-html --title "Sample Diagrams" --source ./doc/samples.md --output ./src/test_samples.html  --corners false --dark-mode false
+markdown-to-html --title "Sample Diagrams" --source ./doc/samples.md --output ./src/test_samples.html  --no-corners --no-dark-mode --markdown-style-theme light
